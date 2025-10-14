cd5419f59ce50e5bbf5bf565a79f1284b2e54407
Elizarov Alexey <judosambo630e@gmail.com>
2025-10-15 01:17:23 +0300

    feat: add Transaction already processed exception

---
+++ dotnet-business/src/FinancialProcessor.Core/Exceptions/TransactionAlreadyProcessedException.cs
@@ -0,0 +1,36 @@
+﻿using FinancialProcessor.Core.Enumerations;
+
+namespace FinancialProcessor.Core.Exceptions
+{
+    internal class TransactionAlreadyProcessedException : DomainException
+    {
+        public Guid TransactionId { get; }
+        public TransactionStatus CurrentStatus { get; }
+
+        public TransactionAlreadyProcessedException(
+            Guid transactionId,
+            TransactionStatus currentStatus)
+            : base(
+                errorCode: "TRANSACTION_ALREADY_PROCESSED",
+                userMessage: $"Transaction cannot be modified because it's already in {currentStatus} status.",
+                technicalDetails: $"TransactionId: {transactionId}, CurrentStatus: {currentStatus}")
+        {
+            TransactionId = transactionId;
+            CurrentStatus = currentStatus;
+        }
+
+        public TransactionAlreadyProcessedException(
+            Guid transactionId,
+            TransactionStatus currentStatus,
+            Exception innerException)
+            : base(
+                errorCode: "TRANSACTION_ALREADY_PROCESSED",
+                userMessage: $"Transaction cannot be modified because it's already in {currentStatus} status.",
+                technicalDetails: $"TransactionId: {transactionId}, CurrentStatus: {currentStatus}",
+                innerException: innerException)
+        {
+            TransactionId = transactionId;
+            CurrentStatus = currentStatus;
+        }
+    }
+}
